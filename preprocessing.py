4d7986c20fe90a17860306ba48e29b081e2b2dcc
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-05-09 13:37:16 +0200

    imported edge_boxes code

--- preprocessing.py
+++ preprocessing.py
@@ -2,9 +2,10 @@ from scipy import misc, ndimage
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from edge_boxes_with_python.edge_boxes import get_windows
 
 BINARY_TH = 200 # binary threshold
-NOISE_SIZE_TH = 5 # threshold for what size counts as noise
+NOISE_SIZE_TH = 3 # threshold for what size counts as noise
 # minimum nr of pixels lines have to be to count as table lines
 MIN_TABLE_SIZE_H = 100
 MIN_TABLE_SIZE_V = 100
@@ -48,10 +49,10 @@ def main():
     line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-003-y1=421-y2=571.pgm')
     line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-005-y1=701-y2=852.pgm') # character touches table line
     line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-007-y1=984-y2=1129.pgm')
-    line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-008-y1=1120-y2=1268.pgm')
-    line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-009-y1=1259-y2=1499.pgm')
-    line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0005-line-001-y1=0-y2=142.pgm') # bad line
-    line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0005-line-003-y1=269-y2=419.pgm')
+    # line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-008-y1=1120-y2=1268.pgm')
+    # line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-009-y1=1259-y2=1499.pgm')
+    # line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0005-line-001-y1=0-y2=142.pgm') # bad line
+    # line = misc.imread('Train/lines+xml/1/navis-Ming-Qing_18341_0005-line-003-y1=269-y2=419.pgm')
     binarize(line)
     test = remove_table_lines(line, 1, MIN_TABLE_SIZE_H) # removes horizontal table lines
     test = remove_table_lines(test, MIN_TABLE_SIZE_V, 1) # removes vertical table lines
