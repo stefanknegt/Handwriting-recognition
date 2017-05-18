27b5a3f860926ea8b1c7f40506424daccc9ca47a
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-05-18 19:03:03 +0200

    horizontal density

--- create_xml.py
+++ create_xml.py
@@ -6,3 +6,11 @@ from preprocessing import *
 def generate_xml(im_file):
     img = misc.imread(im_file)
     img = preprocess_img(img)
+
+    boxes = []
+
+    lines_list, top_bottom, white_space = split_by_density(test, 0)
+
+    for i in range(len(lines_list)):
+        im_list, left_right = split_with_con_comp(lin)
+        boxes.append(top_bottom[i][0], top_bottom[i][1], left_right[0], left_right[1])
