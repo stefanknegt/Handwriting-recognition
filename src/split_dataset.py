2e67d8b66df64805af6ea2846ccb855dce532a63
Rogier de Weert <rogier92@gmail.com>
2017-05-24 16:04:36 +0200

    First upload

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,28 +1,44 @@
 import os
+from shutil import copyfile
 
 MINIMUM_INSTANCES = 100
+TRAIN_SIZE = 0.8
+TEST_SIZE = 1-TRAIN_SIZE
 
 def split():
-    if not os.path.exists('../data/Train/temp'):
-        os.makedirs('../data/Train/temp')
-    if not os.path.exists('../data/Train/train'):
-        os.makedirs('../data/Train/train')
-    if not os.path.exists('../data/Train/test'):
-        os.makedirs('../data/Train/test')
+    if not os.path.exists('../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_train'):
+        os.makedirs('../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_train')
+    if not os.path.exists('../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_test'):
+        os.makedirs('../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_test')
 
-    for updir in os.listdir('../data/Train/128'):
-        path = os.path.join('../data/Train/128', updir)
+    updir = '../data/Train/annotated_crops/128'
+
+    for dir in os.listdir(updir):
+        path = os.path.join(updir, dir)
         leng = len([name for name in os.listdir(path) if os.path.isfile(path + '/'+ name)])
-        if len
-        print len([name for name in os.listdir(path) if os.path.isfile(path + '/'+ name)])
-        count = 0
-        #pa = os.path.join('annotated_crops/128_extended', dir)
-        #for filename in os.listdir(pa):
-        #    pat = os.path.join(pa, filename)
-        #    if os.path.isfile(pat):
-        #        count += 1
-        #        total += 1
-        #txt = txt + ', ' + str(count) + '\n'
+        if leng >= MINIMUM_INSTANCES:
+            point = 0
+            break_point = int(TRAIN_SIZE*leng)
+            print(leng, break_point, leng-break_point)
+            for filename in os.listdir(path):
+                src = os.path.join(path, filename)
+                if point < break_point:
+                    dst = '../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_train/'
+                    dst = os.path.join(dst, dir)
+                    if not os.path.exists(dst):
+                        os.makedirs(dst)
+                else:
+                    dst = '../data/Train/annotated_crops/128_over_' + str(MINIMUM_INSTANCES-1) + '_test/'
+                    dst = os.path.join(dst, dir)
+                    if not os.path.exists(dst):
+                        os.makedirs(dst)
+                dst = os.path.join(dst, filename)
+                copyfile(src, dst)
+                point +=1
 
 if __name__ == '__main__':
+    #if os.path.exists('../data/Train/annotated_crops/128_train'):
+    #    os.remove('../data/Train/annotated_crops/128_train')
+    #if os.path.exists('../data/Train/annotated_crops/128_test'):
+    #    os.remove('../data/Train/annotated_crops/128_test')
     split()
