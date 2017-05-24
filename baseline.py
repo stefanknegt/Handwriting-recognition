41267787ab82c21b3bd2a90bf54f5e906b1c1a2f
Rogier de Weert <rogier92@gmail.com>
2017-05-24 14:43:49 +0200

    added small files for augmentation, now going to work on data aplitting into training and testing sets

--- baseline.py
+++ baseline.py
@@ -50,8 +50,10 @@ def baseline_model_CNN():
     model = Sequential()
     model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Conv2D(15, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
