7c5e908ec8484c99447a34c8b837ae882ae81d29
Rogier de Weert <rogier92@gmail.com>
2017-05-30 10:19:14 +0200

    better loading

--- src/test_model.py
+++ src/test_model.py
@@ -6,7 +6,7 @@ K.set_image_data_format('channels_first')
 
 from keras.models import load_model
 
-input_img=cv2.imread('../data/Train/annotated_crops/128/40e3/')
+input_img=cv2.imread('../data/Train/annotated_crops/128/4e0a/4e0a_aczfkbqulzue.pgm')
 input_img=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
 input_img_resize=cv2.resize(input_img,(128,128))
 
@@ -14,10 +14,10 @@ img_data = np.array(input_img_resize)
 img_data = img_data.astype('float32')
 img_data /= 255
 if K.image_data_format() == 'channels_first':
-    img_data = np.expand_dims(img_data, axis=1)
+    img_data = np.expand_dims(img_data, axis=0)
     print (img_data.shape)
 else:
-    img_data = np.expand_dims(img_data, axis=4)
+    img_data = np.expand_dims(img_data, axis=2)
     print (img_data.shape)
 test_image = img_data
 
