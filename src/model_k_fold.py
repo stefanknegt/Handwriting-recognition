1e784fb2c3b02a802e475d9df1de9a6c279e6986
Rogier de Weert <rogier92@gmail.com>
2017-06-07 10:32:17 +0200

    added 128_extended_bin (554MB)

--- src/model_k_fold.py
+++ src/model_k_fold.py
@@ -61,6 +61,7 @@ def load_and_shuffle(data_path, verbose):
         j += 1
 
     img_data = np.array(img_data_list)
+    del img_data_list
     img_data = img_data.astype('float32')
     img_data /= 255
     print('Input dimensions of all data: ' + str(img_data.shape))
@@ -81,20 +82,23 @@ def load_and_shuffle(data_path, verbose):
     # Label creation
     num_of_samples = img_data.shape[0]
     labels = np.ones((num_of_samples,), dtype='int64')
-    names = []
+    #names = []
 
     i = 0
     j = 0
 
     for dataset in data_dir_list:
-        names.append(dataset)
+        #names.append(dataset)
         img_list = os.listdir(data_path + '/' + dataset)
         for i in range(len(img_list) - invalid_ims[j]):
             labels[i] = j
         j += 1
-
+        
+    del data_dir_list, i, j
+    
     # convert class labels to on-hot encoding
     Y = np_utils.to_categorical(labels, num_classes)
+    del labels
 
     print('Randomly shuffling data')
     # Scary unison shuffle!
@@ -102,6 +106,7 @@ def load_and_shuffle(data_path, verbose):
     np.random.shuffle(img_data)
     np.random.set_state(rng_state)
     np.random.shuffle(Y)
+    del rng_state
 
     input_shape = img_data[0].shape
 
@@ -142,7 +147,7 @@ def train_test_evaluate(num_classes, input_shape, X_train, y_train, X_test, y_te
 
     print('Start training...')
     # Fit the model
-    early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=1)
     hist = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=num_epoch, batch_size=100, verbose=2, callbacks=[early_stopping])
 
     # Final evaluation of the model
