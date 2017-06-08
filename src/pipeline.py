906a9c1285f2891a6e6950a90b71e7cf645c3801
Stefan Knegt <stefan@RN-145-97-148-243.eduroam.rug.nl>
2017-06-08 12:44:04 +0200

    pipeline extended with plot

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,6 +2,7 @@ import os, cv2, numpy as np
 from keras.models import load_model
 from load_data import load_test_data
 from keras.utils import np_utils
+from matplotlib import pyplot
 
 from keras import backend as K
 if K.backend()=='tensorflow':
@@ -17,7 +18,7 @@ model.summary()
 
 #num_classes, _, _, _, X_test, y_test = load_data_internal('128', verbose = False) #Hier komt een aparte functie voor alleen test data
 X_test = load_test_data('test_set_128_bin', verbose=False)
-X_test = load_test_data('test_set_128_extended_bin', verbose=False)
+#X_test = load_test_data('test_set_128_extended_bin', verbose=False)
 #print(num_classes)
 print('Shape of testing data: '+ str(X_test.shape))
 #print('Shape of testing labels: '+ str(y_test.shape))
@@ -31,10 +32,14 @@ print('Shape of testing data: '+ str(X_test.shape))
 predictions = model.predict(X_test, batch_size=50, verbose=0)
 #pred = np_utils.to_categorical(predictions, num_classes)
 
+entropies = []
+
 for i in range(0,len(predictions)):
     predicted_class_model1 = np.argmax(predictions[i])
     entropy_model1 = predictions[i][predicted_class_model1]
+    entropies.append(entropy_model1)
     if(entropy_model1 < 0.99):
+        #entropies.append(entropy_model1)
         print(entropy_model1)
         #Call hier tweede model
         #if(entropy_model1 < entropy_model2): Dit is niet fair omdat tweede model meer classes heeft!
@@ -47,3 +52,8 @@ for i in range(0,len(predictions)):
 
 #accuracy = (np.count_nonzero(pred!=y_test)/(predictions.shape[0]*2))*100
 #print("Model accuracy: "+ str(accuracy)+ '%')
+print(np.average(entropies))
+print(np.mean(entropies))
+pyplot.hist(entropies)
+pyplot.axvline(np.mean(entropies), color='b', linestyle='dashed', linewidth=2)
+pyplot.show()
