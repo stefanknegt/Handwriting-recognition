29b85a81c08171ece29d569586dd54bac21064ba
Rogier de Weert <rogier92@gmail.com>
2017-06-21 15:36:03 +0200

    cleaning up preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -17,41 +17,11 @@ SPLIT_TH = 0
 OVERLAP_TH = 0.1
 DEBUG = True
 
-def binarize(img):
-    '''turns gray scale image into binary based on threshold'''
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
-            if img[i][j] <= BINARY_TH:
-                img[i][j] = 0
-            else:
-                img[i][j] = 1
-    return img
-
-
 def binarize_otsu(img):
     "Turns greyscale image into binary using Otsu's method"
     val = filters.threshold_otsu(img)
     mask = img > val
     return mask
-
-def rotate_lines(img):
-    "Rotates lines to make the characters line up horizontally for further segmentation"
-    hor = []
-    vert = []
-    for i in range(0, img.shape[0]):
-        for j in range(0, img.shape[1]):
-            if img[i][j] < 1:
-                hor.append(j)
-                vert.append(i)
-
-    pixels = np.array([hor, vert])
-    cov = np.cov(pixels)
-    print(cov.tolist())
-    w, v = np.linalg.eig(cov)
-    angle = math.atan2(v[0,1], v[1,1])
-    print(angle)
-    rotated_img = ndimage.rotate(img, math.degrees(-angle), order=1)
-    return rotated_img
 
 def remove_table_lines(img, x, y):
     '''Removes horizontal or vertical table lines'''
@@ -64,7 +34,6 @@ def remove_table_lines(img, x, y):
     recon_image = np.logical_not(recon_image)
     res = np.logical_not(img - recon_image)
     return res
-
 
 def remove_noise(img, threshold, inv=True):
     '''closing/opening by reconstruction'''
@@ -203,8 +172,7 @@ def remove_whitespace_top_bottom(img):
     ud_img = np.delete(ud_img, np.arange(i), 0)
     img = np.flipud(ud_img)
     return img
-    
-    
+
 def update_top_bottom(img, top_bottom):
     '''This removes the whitespace from top and bottom of an img (np-array) and returns the updated top and bottom as a tuple'''
     for i in range(0,img.shape[0]):
@@ -227,7 +195,6 @@ def update_top_bottom(img, top_bottom):
     img = np.flipud(ud_img)
     top_bottom = (top_bottom[0] + top_offset, top_bottom[1] - bot_offset) # add offsets to top and bottom ycoords
     return img, top_bottom 
-
 
 def calculate_sizes(char_list):
     sizes = np.zeros((len(char_list),2), dtype=np.int)
@@ -304,7 +271,6 @@ def sizes(image, rotate, output):
 
     return new_image
 
-
 def preprocess_img(img):
     img = binarize_otsu(img)
     img = remove_table_lines(img, 1, MIN_TABLE_SIZE_H)
@@ -312,79 +278,31 @@ def preprocess_img(img):
     img = remove_noise(img, NOISE_SIZE_TH)
     return img
 
-
 def main():
-    line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm')
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-002-y1=280-y2=430.pgm')  # character touches table line
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-003-y1=421-y2=571.pgm')
-    # line = misc.imread(
-    #     '../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-005-y1=701-y2=852.pgm')  # character touches table line
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-007-y1=984-y2=1129.pgm')
-    # # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-009-y1=1259-y2=1499.pgm')
     line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-003-y1=421-y2=571.pgm') # character touches table line
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-007-y1=984-y2=1129.pgm')
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-008-y1=1120-y2=1268.pgm')
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-009-y1=1259-y2=1499.pgm')
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18637_0002-line-003-y1=343-y2=508.pgm') # background gray
-    # line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18637_0022-line-009-y1=1226-y2=1386.pgm') # tilted
-    if DEBUG:
-        fig = plt.figure()
-        a = fig.add_subplot(3, 1, 1)
-        plt.imshow(line, cmap=plt.cm.gray)
-        otsu = binarize_otsu(line)
-        normal = binarize(line)
-        a = fig.add_subplot(3, 1, 2)
-        plt.imshow(normal, cmap=plt.cm.gray)
-        a = fig.add_subplot(3, 1, 3)
-        plt.imshow(otsu, cmap=plt.cm.gray)
-        plt.show()
+
     otsu = binarize_otsu(line)
-    t = rotate_lines(otsu)
     test = remove_table_lines(otsu, 1, MIN_TABLE_SIZE_H)  # removes horizontal table lines
     test = remove_table_lines(test, MIN_TABLE_SIZE_V, 1)  # removes vertical table lines
     test = remove_noise(test, NOISE_SIZE_TH)
 
-    test1 = remove_table_lines(t, 1, MIN_TABLE_SIZE_H)  # removes horizontal table lines
-    test1 = remove_table_lines(test1, MIN_TABLE_SIZE_V, 1)  # removes vertical table lines
-    test1 = remove_noise(test1, NOISE_SIZE_TH)
-
     if DEBUG:
         fig = plt.figure()
-        a = fig.add_subplot(4, 1, 1)
-        plt.imshow(t, cmap=plt.cm.gray)
-        a = fig.add_subplot(4, 1, 2)
-        plt.imshow(test1, cmap=plt.cm.gray)
-        a = fig.add_subplot(4, 1, 3)
+        a = fig.add_subplot(3, 1, 1)
+        plt.imshow(line, cmap=plt.cm.gray)
+        a = fig.add_subplot(3, 1, 2)
         plt.imshow(otsu, cmap=plt.cm.gray)
-        a = fig.add_subplot(4, 1, 4)
+        a = fig.add_subplot(3, 1, 3)
         plt.imshow(test, cmap=plt.cm.gray)
         plt.show()
 
-    # h_hist = density_plot(test, 0)
-    # plt.plot(h_hist)
-    plt.imshow(test, cmap=plt.cm.gray, vmin=0, vmax=1)
-    plt.show()
     lines_list, lines, white_space = split_by_density(test, 0)
 
-    if True:
-        char_list = []
-        for lin in lines_list:
-            plt.imshow(lin, cmap=plt.cm.gray)
-            plt.show()
-            im_list, lines = split_with_con_comp(lin)
-            char_list.extend(im_list)
+    char_list = []
+    for lin in lines_list:
+        im_list, lines = split_with_con_comp(lin)
+        char_list.extend(im_list)
 
-        # plt.imshow(test, cmap=plt.cm.gray, vmin=0, vmax=1)
-        # for val in lines_a:
-        #     plt.plot([val, val], [0, line.shape[0]], 'b')
-        # plt.show()
-        # plt.imshow(test, cmap=plt.cm.gray, vmin=0, vmax=1)
-        # for val in lines_b:
-        #     plt.plot([val, val], [0, line.shape[0]], 'r')
-        # plt.show()
-
-    # new_list = []
-    # print(new_list)
     for image in char_list:
         new_img = remove_whitespace_top_bottom(image)
         # new_list.insert(0,new_img)
