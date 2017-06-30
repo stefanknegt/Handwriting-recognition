1dddcef0c64d1e01285e9696eb9e6d1aad213c22
TimOosterhuis <tim.oosterhuis@hotmail.com>
2017-06-30 23:04:28 +0200

    added comments to preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,41 +8,52 @@ try:
 except ImportError:
     from skimage import filter as filters
 
-BINARY_TH = 200  # binary threshold
-NOISE_SIZE_TH = 3  # threshold for what size counts as noise
-# minimum nr of pixels lines have to be to count as table lines
-MIN_TABLE_SIZE_H = 100
-MIN_TABLE_SIZE_V = 100
-SPLIT_TH = 0
-OVERLAP_TH = 0.1
-DEBUG = False
-PLOT = False
+NOISE_SIZE_TH = 3       # threshold for what size counts as noise
+MIN_TABLE_SIZE_H = 100  # minimum nr of pixels horiz. lines have to be to count as table lines
+MIN_TABLE_SIZE_V = 100  # minimum nr of pixels vert. lines have to be to count as table lines
+SPLIT_TH = 0            # threshold black pixel density for pixel density splitting
+OVERLAP_TH = 0.1        # threshold for percentage of overlap for splitting based on connected component overlap
+DEBUG = False           # boolean to log debugging statements
+PLOT = False            # boolean to draw image plots
 
 def binarize_otsu(img):
-    "Turns greyscale image into binary using Otsu's method"
+    '''Turns greyscale image into binary using Otsu's method.
+    
+    parameters
+    img:    a grayscale image
+    
+    returns
+    mask:   an otsu thresholded binary image
+    '''
     val = filters.threshold_otsu(img)
     mask = img > val
     return mask
 
 def remove_table_lines(img, x, y):
-    '''Removes horizontal or vertical table lines'''
+    '''Removes horizontal or vertical table lines with morphological image operations.
+    
+    parameters 
+    img:    an input image which potentially contains table lines
+    x, y:   the dimensions of the erosion structuring element, one of these values will be the min. length
+            an uninterrupted straight line has to have to be considered a table line, the other will be one
+    
+    returns
+    res:    a version of the image with the identified table lines removed
+    '''
     img_inv = np.logical_not(img)
-    str_e = np.ones((x, y))
-    # parameters voor het str_d element waarmee otsu net zo goed werkt voor het removen van de tablelines als global threshold van 254:
+    str_e = np.ones((x, y)) 
     if x > y:
-        str_d = np.ones((x*10, y+7)) # dilate with a slightly larger structuring element than for the erosion to get rid of irregular table lines a bit better
+        str_d = np.ones((x*10, y+7)) # dilate with a larger str. element than the one for the erosion
     else:
         str_d = np.ones((x+7, y*10))
     eroded = ndimage.binary_erosion(img_inv, structure=str_e).astype(img_inv.dtype)
-    # recon_image = ndimage.binary_propagation(eroded, mask=img_inv)
-    recon_image = ndimage.binary_dilation(eroded, structure=str_d).astype(eroded.dtype)
-    #recon_image = np.logical_not(recon_image)
-    #res = np.logical_not(img - recon_image)
-    res = np.logical_or(img, recon_image)
+    # here recon_image will be a black image with a white region around the location of each identified table line
+    recon_image = ndimage.binary_dilation(eroded, structure=str_d).astype(eroded.dtype) 
+    res = np.logical_or(img, recon_image) # black pixels within the table line region(s) are removed
     return res
     
 def remove_table_lines2(img, ref_img, x, y):
-    '''Removes horizontal or vertical table lines'''
+    '''Removes horizontal or vertical table line GAAN WE NIET INLEVEREN GA IK NOG WEL GEBRUIKEN OM RESULTATEN TE KRIJGEN VOOR IOUS'''
     bin_img = ref_img > 254
     img_inv = np.logical_not(bin_img)
     str_e = np.ones((x, y))
@@ -55,7 +66,18 @@ def remove_table_lines2(img, ref_img, x, y):
     return res    
 
 def remove_noise(img, threshold, inv=True):
-    '''closing/opening by reconstruction'''
+    '''Removes small noise pixels using morphological closing or opening by reconstruction
+    
+    parameters
+    img:        a noisy image
+    threshold:  threshold size for what is seen as noise and what is seen as an object in the image
+                (size of the structuring disk)
+    inv:        a boolean if true a closing by reconstruction is performed else an opening by reconstruction
+                (inv=True or False removes small black or white noise specs respectively)  
+
+    returns
+    recon_image    
+    '''
     if inv:
         img = np.logical_not(img)
     str_e = np.ones((threshold, threshold))
@@ -66,7 +88,15 @@ def remove_noise(img, threshold, inv=True):
     return recon_image
 
 def density_plot(img, axis):
-    '''takes an image, and returns a histogram of the amount of pixels per column if axis==1 else per row'''
+    '''Makes a histogram of the amount of pixels per row or column of an image.
+    
+    parameters
+    img:    the input image
+    axis:   the axis for which to make the histogram, 0 for the horizontal axis 1 for the vertical
+    
+    returns
+    hist:   a density histogram of the number of pixels per row/column on the axis specified by 'axis'
+    '''
     hist = [0] * img.shape[axis]
     for i in range(0, img.shape[axis]):
         for j in range(0, img.shape[1 - axis]):
@@ -77,22 +107,32 @@ def density_plot(img, axis):
     return hist
 
 def split_by_density(img, axis):
-    '''split image based on axis density with a threshold. 0 for horizontal density, 1 for vertical'''
+    '''Splits an image of a line of characters into individual characters based on an axis pixel density, 
+    
+    parameters
+    img:            the input image
+    axis:           the axis for which to make the histogram, 0 for the horizontal axis 1 for the vertical
+    
+    returns 
+    img_lst:        a list of character image segments (numpy arrays) 
+    lines:          a list of begin and end coordinates of the image segments within the larger line image
+    white_space:    a list of the number of white spaces between each of the image segments
+    '''
     hist = density_plot(img, axis)
     img_lst = []
     lines = []
     image_flag = False
     white_space = []
     w = 0
+    # move through the rows/cols of the image
     for i in range(0, len(hist)):
-        if not image_flag and hist[i] > SPLIT_TH:
+        if not image_flag and hist[i] > SPLIT_TH: # beginning of a new character image segment
             im_start = i
             image_flag = True
-            #lines.append(i)
             #track nr of whitespaces previous to image segment
             white_space.append(w)
             w = 1
-        elif image_flag and (hist[i] <= SPLIT_TH or i + 1 == len(hist)):
+        elif image_flag and (hist[i] <= SPLIT_TH or i + 1 == len(hist)): # end of a character image segment
             im_stop = i
             image_flag = False
             if axis == 1:
@@ -100,11 +140,19 @@ def split_by_density(img, axis):
             else:
                 img_lst.append(img[im_start:im_stop, :])
             lines.append((im_start, im_stop))
-        elif not image_flag:
+        elif not image_flag: # outsite of an character image segment
             w += 1
     return img_lst, lines, white_space
 
 class Component:
+    '''A class for a connected component within an image segment
+    
+    properties
+    start:  the x coordinate where the component starts
+    stop:   the x coordinate where the component stops
+    size:   the length of the component in the x dimension
+    label:  the character label of the component, components with the same label belong to the same character
+    '''
     def __init__(self, start, stop):
         self.start = start
         self.stop = stop
@@ -112,7 +160,16 @@ class Component:
         self.label = None
 
 def split_with_con_comp(img):
-    # hist = density_plot(img, 1)
+    '''Splits an image of a line of characters into individual characters based on clusterings of overlapping connected components. 
+    
+    parameters
+    img:            the input image
+    
+    returns
+    img_lst_new:    a list of character image segments (numpy arrays) 
+    lines_lst_new:  a list of begin and end coordinates of the image segments within the larger line image
+    '''
+    # Call on split_by_density for the initial split
     img_lst, lines, wh_sp = split_by_density(img, 1)
     img_lst_new = []
     lines_lst_new = []
@@ -123,10 +180,11 @@ def split_with_con_comp(img):
         if image.shape[1] < image.shape[0]:
             img_lst_new.append(image)
             lines_lst_new.append(lines[i])
-        else:
+        else: # the image segment is so wide that it is likely to contain multiple characters
             if DEBUG:
                 print('trying to split wide image')
 
+            # create a labeling of all connected components within the image segment and sort them by their size
             img_inv = np.logical_not(image)
             labels, n_labels = ndimage.label(img_inv)
             mask = image < image.mean()
@@ -134,22 +192,20 @@ def split_with_con_comp(img):
             ordered_labels = np.argsort(sizes)
             sz = [int(x) for x in sizes.tolist()[1:]]
             ordered_labels = [i[0] for i in sorted(enumerate(sz), key=lambda x:x[1], reverse=True)]
-            if DEBUG:
-                print(sz)
-                print(ordered_labels)
+
             label_i = 1
             components = []
-            for i in range(1, n_labels + 1):
+            for i in range(1, n_labels + 1): # go through all connected components starting with the largest
                 slice_x, slice_y = ndimage.find_objects(labels == ordered_labels[i - 1] + 1)[0]
                 new_comp = Component(slice_y.start, slice_y.stop)
-                if DEBUG:
-                    print('new comp : ' + str(new_comp.__dict__))
+                
+                # assign a cluster label to the largest component
                 if not components:
                     new_comp.label = label_i
-
-                for comp in components:
-                    if DEBUG:
-                        print('compare with comp : ' + str(comp.__dict__))
+                
+                # compare each new component with all previous components (starting with the largest) and assign it the same class label as the first component it 
+                # has enough overlap with
+                for comp in components: 
                     if new_comp.start < comp.start:
                         if new_comp.stop > comp.start:
                             if DEBUG:
@@ -164,15 +220,13 @@ def split_with_con_comp(img):
                             if (comp.stop - new_comp.start) / float(min(new_comp.size, comp.size)) > OVERLAP_TH:
                                 new_comp.label = comp.label
                                 break
+                # if the component doesn't have an overlap (as determined by the threshold) with any of the previous components assign it a new class label
                 if not new_comp.label:
-                    if DEBUG:
-                        print('fits with none, new label')
                     label_i += 1
                     new_comp.label = label_i
                 components.append(new_comp)
-                if DEBUG:
-                    print(new_comp.__dict__)
 
+            # find the beginning and ending of the characters as defined by their new class labels
             min_max = [list([9999, 0])] * label_i
             for comp in components:
                 if comp.start < min_max[comp.label - 1][0]:
@@ -189,7 +243,16 @@ def split_with_con_comp(img):
     return img_lst_new, lines_lst_new
 
 def update_top_bottom(img, top_bottom):
-    '''This removes the whitespace from top and bottom of an img (np-array) and returns the updated top and bottom as a tuple'''
+    '''Crops the whitespace from top and bottom of a character image
+    
+    parameters
+    img:        a character input image
+    top_bottom: the current top and bottom y coordinates of the character in a larger line image
+    
+    returns 
+    img:        the cropped image
+    top_bottom: the updated top and bottom y coordinates
+    '''
     for i in range(0,img.shape[0]):
         for j in range(0, img.shape[1]):
             if img[i][j]!=1:
@@ -212,6 +275,7 @@ def update_top_bottom(img, top_bottom):
     return img, top_bottom 
 
 def sizes(image, rotate, output):
+    '''Resizes images to be 128 by 128 pixels?'''
     if rotate:
         image = np.rot90(image, 3)
     edge = int((output/32.0)/2.0)
@@ -262,6 +326,16 @@ def sizes(image, rotate, output):
     return new_image
 
 def combine_small(boxes, small_w = 35, small_h = 15):
+    '''Combines or appends small loose character strokes that are oversegmented when an image split by density
+    
+    parameters
+    boxes:      a lsit of bounding boxes of characters in a line
+    small_w:    width threshold
+    small_h:    height threshold
+    
+    returns
+    boxes2:     updated list of bounding boxes
+    '''
     if DEBUG:
         print('Searching for small characters')
     def getKey(item):
@@ -273,7 +347,7 @@ def combine_small(boxes, small_w = 35, small_h = 15):
         if skip:
             skip = False
             continue
-        if boxes[i][2] <= small_w: # Width of a box is below 30
+        if boxes[i][2] <= small_w: # Width of a box is below the threshold
             if boxes[i][3] <= small_h:
                 continue
             if len(boxes2)==0: # First char, check only with second char
@@ -335,14 +409,24 @@ def combine_small(boxes, small_w = 35, small_h = 15):
     return boxes2
 
 def process_for_classification(img):
+    '''Preprocesses and segments an image of a line of characters
+    
+    parameters
+    img:            the input image
+    
+    returns
+    final_image:    a list of character images (numpy arrays)
+    boxes:          a list of bounding boxes for the characters within the original image
+    '''
     otsu = binarize_otsu(img)
+    
+    test = remove_noise(test, NOISE_SIZE_TH, inv=False)
+    
     test = remove_table_lines(otsu, 1, MIN_TABLE_SIZE_H)
     test = remove_table_lines(test, MIN_TABLE_SIZE_V, 1)
 
-    #test = remove_table_lines2(otsu, img, 1, MIN_TABLE_SIZE_H)
-    #test = remove_table_lines2(test, img, MIN_TABLE_SIZE_V, 1)
-
     test = remove_noise(test, NOISE_SIZE_TH)
+
 
     if DEBUG and PLOT:
         fig = plt.figure()
@@ -396,9 +480,8 @@ def process_for_classification(img):
             ax2.add_patch(rect)
         plt.show()
 
-    '''
-    Extract images from boxes
-    '''
+    
+    # Extract images from boxes
     final_images = np.zeros((len(boxes), 128, 128))
     i = 0
     for box in boxes:
@@ -416,9 +499,6 @@ def process_for_classification(img):
 
             fig.add_subplot(2, 1, 1)
             plt.imshow(image, cmap=plt.cm.gray, vmin=0, vmax=1)
-            #h_hist = density_plot(image, 0)
-            #x = np.arange(len(h_hist))
-            #plt.plot(h_hist, x)
 
             fig.add_subplot(2, 1, 2)
             plt.imshow(final_img, cmap=plt.cm.gray, vmin=0, vmax=1)
@@ -427,20 +507,22 @@ def process_for_classification(img):
     return boxes, final_images
 
 def main():
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-003-y1=421-y2=571.pgm') # small last character
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm')
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0006-line-009-y1=1224-y2=1377.pgm')
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0069-line-003-y1=258-y2=388.pgm')
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18637_0017-line-008-y1=1082-y2=1244.pgm')  # Still table lines left
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0010-line-006-y1=997-y2=1321.pgm') # Only four chars
-    #process_for_classification(line)
-    #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0028-line-005-y1=574-y2=716.pgm')
-    #process_for_classification(line)
+    '''The preprocessing main can be used to call process_for_classification on a single example line
+    
+    Set PLOT to True
+       
+    make sure the path in misc.imread() points to an existing image
+    
+    examples:
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-003-y1=421-y2=571.pgm') # small last character
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0004-line-001-y1=0-y2=289.pgm')
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18341_0006-line-009-y1=1224-y2=1377.pgm')
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0069-line-003-y1=258-y2=388.pgm')
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18637_0017-line-008-y1=1082-y2=1244.pgm')  # Still table lines left
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0010-line-006-y1=997-y2=1321.pgm') # Only four chars
+       #line = misc.imread('../data/Train/lines+xml/1/navis-Ming-Qing_18641_0028-line-005-y1=574-y2=716.pgm')
+    '''
+    
     line = misc.imread('../data/Train/lines+xml/7/navis-Ming-Qing_HarvYench_18_10_10084_0015-line-002-y1=509-y2=637.pgm')
     process_for_classification(line)
 
